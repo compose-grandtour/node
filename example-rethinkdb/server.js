d19ea687ab2a74b279f7d61d8270490ea48954dd
Dj Walker-Morgan <codepope@gmail.com>
2018-02-21 14:00:00 +0000

    Tidy and rework

--- example-rethinkdb/server.js
+++ example-rethinkdb/server.js
@@ -9,9 +9,9 @@ const fs = require("fs");
 // Use body-parser to handle the PUT data
 const bodyParser = require("body-parser");
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+    bodyParser.urlencoded({
+        extended: false
+    })
 );
 
 // We want to extract the port to publish our app on
@@ -27,84 +27,95 @@ let parseRethinkdbUrl = require("parse-rethinkdb-url");
 let connectionString = process.env.COMPOSE_RETHINKDB_URL;
 
 if (connectionString === undefined) {
-  console.error("Please set the COMPOSE_RETHINKDB_URL environment variable");
-  process.exit(1);
+    console.error("Please set the COMPOSE_RETHINKDB_URL environment variable");
+    process.exit(1);
 }
 
 let options = parseRethinkdbUrl(connectionString);
-
 let connection;
 
 // Make the database connection using the parsed options
 // and the SSL certificate, and create the 'examples' database.
 // The SSL certificate is available from the deployment overview page
 // If the database already exists RethinkDB returns an error, which will appear in the console
-let caCert = fs.readFile(process.env.PATH_TO_RETHINKDB_CERT, function(
-  err,
-  caCert
-) {
-  // Now we can insert the SSL credentials
-  options.ssl = {
+let caCert = fs.readFileSync(process.env.PATH_TO_RETHINKDB_CERT);
+
+// Now we can insert the SSL credentials
+options.ssl = {
     ca: caCert
-  };
-  console.log(options);
-  r.connect(options, function(error, conn) {
-    if (error) throw error;
-    else {
-      connection = conn;
-      r.dbCreate("examples").run(connection);
-      r
-        .db("examples")
-        .tableCreate("words")
-        .run(connection);
-    }
-  });
+};
+
+r.connect(options).then(function(conn) {
+    connection = conn;
+}).then(function(x) {
+    return r.dbList().contains("grand_tour").do(
+        function(exists) {
+            return r.branch(exists, { "dbs_created": 0 },
+                r.dbCreate("grand_tour"));
+        }).run(connection);
+}).then(function(result) {
+    if (result.dbs_created > 0) { console.log("DB created"); }
+    return r.db("grand_tour").tableList().contains("words").do(
+        function(exists) {
+            return r.branch(exists, { "tables_created": 0 },
+                r.db("grand_tour").tableCreate("words", { replicas: 3 })
+            );
+        }).run(connection);
+}).then(function(result) {
+    if (result.tables_created > 0) { console.log("Table created"); }
+}).catch(function(err) {
+    console.err(err);
 });
 
+
+
+
+
+
 // Add a word to the database
-function addWord(request) {
-  return new Promise(function(resolve, reject) {
-    r
-      .db("examples")
-      .table("words")
-      .insert({
-        word: request.body.word,
-        definition: request.body.definition
-      })
-      .run(connection, function(error, cursor) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(cursor);
-        }
-      });
-  });
+function addWord(word, definition) {
+    return new Promise(function(resolve, reject) {
+        r
+            .db("grand_tour")
+            .table("words")
+            .insert({
+                word: word,
+                definition: definition
+            })
+            .run(connection, function(error, cursor) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(cursor);
+                }
+            });
+    });
 }
 
 // Get words from the database
 function getWords() {
-  return new Promise(function(resolve, reject) {
-    // we make a database request for the contents of the 'words' table
-    // ordering the results alphabetically
-    r
-      .db("examples")
-      .table("words")
-      .orderBy("word")
-      .run(connection, function(err, cursor) {
-        if (err) {
-          reject(err);
-        } else {
-          // then we convert the response to an array and send it back to 'main.js'
-          cursor.toArray(function(err, results) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(results);
-            }
-          });
-        }
-      });
-  });
+    return new Promise(function(resolve, reject) {
+        // we make a database request for the contents of the 'words' table
+        // ordering the results alphabetically
+        r
+            .db("grand_tour")
+            .table("words")
+            .orderBy("word")
+            .run(connection, function(err, cursor) {
+                if (err) {
+                    reject(err);
+                } else {
+                    // then we convert the response to an array and send it back to 'main.js'
+                    cursor.toArray(function(err, results) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(results);
+                        }
+                    });
+                }
+            });
+    });
 }
 
 // With the database going to be open as some point in the future, we can
@@ -114,30 +125,30 @@ app.use(express.static(__dirname + "/public"));
 // The user has clicked submit to add a word and definition to the database
 // Send the data to the addWord function and send a response if successful
 app.put("/words", function(request, response) {
-  addWord(request)
-    .then(function(resp) {
-      response.send(resp);
-    })
-    .catch(function(err) {
-      console.log(err);
-      response.status(500).send(err);
-    });
+    addWord(request.body.word, request.body.definition)
+        .then(function(resp) {
+            response.send(resp);
+        })
+        .catch(function(err) {
+            console.log(err);
+            response.status(500).send(err);
+        });
 });
 
 // Read from the database when the page is loaded or after a word is successfully added
 // Use the getWords function to get a list of words and definitions from the database
 app.get("/words", function(request, response) {
-  getWords()
-    .then(function(words) {
-      response.send(words);
-    })
-    .catch(function(err) {
-      console.log(err);
-      response.status(500).send(err);
-    });
+    getWords()
+        .then(function(words) {
+            response.send(words);
+        })
+        .catch(function(err) {
+            console.log(err);
+            response.status(500).send(err);
+        });
 });
 
 // Listen for a connection.
 app.listen(port, function() {
-  console.log("Server is listening on port " + port);
+    console.log("Server is listening on port " + port);
 });
