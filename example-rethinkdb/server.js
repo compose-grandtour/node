f17b569c0076df3f9aac350c9e9a57eae0633286
Dj Walker-Morgan <codepope@gmail.com>
2018-03-13 14:20:41 +0000

    Promisfied, arrowed and formatted

--- example-rethinkdb/server.js
+++ example-rethinkdb/server.js
@@ -9,9 +9,9 @@ const fs = require("fs");
 // Use body-parser to handle the PUT data
 const bodyParser = require("body-parser");
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 // We want to extract the port to publish our app on
@@ -27,8 +27,8 @@ let parseRethinkdbUrl = require("parse-rethinkdb-url");
 let connectionString = process.env.COMPOSE_RETHINKDB_URL;
 
 if (connectionString === undefined) {
-    console.error("Please set the COMPOSE_RETHINKDB_URL environment variable");
-    process.exit(1);
+  console.error("Please set the COMPOSE_RETHINKDB_URL environment variable");
+  process.exit(1);
 }
 
 let options = parseRethinkdbUrl(connectionString);
@@ -42,74 +42,71 @@ let caCert = fs.readFileSync(process.env.PATH_TO_RETHINKDB_CERT);
 
 // Now we can insert the SSL credentials
 options.ssl = {
-    ca: caCert
+  ca: caCert
 };
 
-r.connect(options).then(function(conn) {
+r
+  .connect(options)
+  .then(function(conn) {
     connection = conn;
-}).then(function(x) {
-    return r.dbList().contains("grand_tour").do(
-        function(exists) {
-            return r.branch(exists, { "dbs_created": 0 },
-                r.dbCreate("grand_tour"));
-        }).run(connection);
-}).then(function(result) {
-    if (result.dbs_created > 0) { console.log("DB created"); }
-    return r.db("grand_tour").tableList().contains("words").do(
-        function(exists) {
-            return r.branch(exists, { "tables_created": 0 },
-                r.db("grand_tour").tableCreate("words", { replicas: 3 })
-            );
-        }).run(connection);
-}).then(function(result) {
-    if (result.tables_created > 0) { console.log("Table created"); }
-}).catch(function(err) {
+  })
+  .then(() => {
+    return r
+      .dbList()
+      .contains("grand_tour")
+      .do(function(exists) {
+        return r.branch(exists, { dbs_created: 0 }, r.dbCreate("grand_tour"));
+      })
+      .run(connection);
+  })
+  .then(result => {
+    if (result.dbs_created > 0) {
+      console.log("DB created");
+    }
+    return r
+      .db("grand_tour")
+      .tableList()
+      .contains("words")
+      .do(exists => {
+        return r.branch(
+          exists,
+          { tables_created: 0 },
+          r.db("grand_tour").tableCreate("words", { replicas: 3 })
+        );
+      })
+      .run(connection);
+  })
+  .then(result => {
+    if (result.tables_created > 0) {
+      console.log("Table created");
+    }
+  })
+  .catch(err => {
     console.error(err);
-});
+  });
 
 // Add a word to the database
 function addWord(word, definition) {
-    return new Promise(function(resolve, reject) {
-        r
-            .db("grand_tour")
-            .table("words")
-            .insert({
-                word: word,
-                definition: definition
-            })
-            .run(connection, function(error, cursor) {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(cursor);
-                }
-            });
-    });
+  return r
+    .db("grand_tour")
+    .table("words")
+    .insert({
+      word: word,
+      definition: definition
+    })
+    .run(connection);
 }
 
 // Get words from the database
 function getWords() {
-    return new Promise(function(resolve, reject) {
-        // we make a database request for the contents of the 'words' table
-        // ordering the results alphabetically
-        r
-            .db("grand_tour")
-            .table("words")
-            .orderBy("word")
-            .run(connection, function(err, cursor) {
-                if (err) {
-                    reject(err);
-                } else {
-                    // then we convert the response to an array and send it back to 'main.js'
-                    cursor.toArray(function(err, results) {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve(results);
-                        }
-                    });
-                }
-            });
+  return r
+    .db("grand_tour")
+    .table("words")
+    .orderBy("word")
+    .run(connection)
+    .then(cursor => {
+      // then we convert the response to an array and send it back to 'main.js'
+      return cursor.toArray();
     });
 }
 
@@ -120,30 +117,30 @@ app.use(express.static(__dirname + "/public"));
 // The user has clicked submit to add a word and definition to the database
 // Send the data to the addWord function and send a response if successful
 app.put("/words", function(request, response) {
-    addWord(request.body.word, request.body.definition)
-        .then(function(resp) {
-            response.send(resp);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
+  addWord(request.body.word, request.body.definition)
+    .then(function(resp) {
+      response.status(200).send(resp);
+    })
+    .catch(function(err) {
+      console.log(err);
+      response.status(500).send(err);
+    });
 });
 
 // Read from the database when the page is loaded or after a word is successfully added
 // Use the getWords function to get a list of words and definitions from the database
 app.get("/words", function(request, response) {
-    getWords()
-        .then(function(words) {
-            response.send(words);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
+  getWords()
+    .then(function(words) {
+      response.send(words);
+    })
+    .catch(function(err) {
+      console.log(err);
+      response.status(500).send(err);
+    });
 });
 
 // Listen for a connection.
 app.listen(port, function() {
-    console.log("Server is listening on port " + port);
+  console.log("Server is listening on port " + port);
 });
