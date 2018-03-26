d418d1e15b692c2c55944114cc1866fa2c9835ec
Dj Walker-Morgan <codepope@gmail.com>
2018-03-26 14:42:24 +0100

    Final, final format (cough)

--- example-scylla/server.js
+++ example-scylla/server.js
@@ -8,9 +8,9 @@ const app = express();
 // Use body-parser to handle the PUT data
 const bodyParser = require("body-parser");
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 // We want to extract the port to publish our app on
@@ -30,8 +30,8 @@ const compose = require("composeaddresstranslator");
 let connectionString = process.env.COMPOSE_SCYLLA_URLS;
 
 if (connectionString === undefined) {
-    console.error("Please set the COMPOSE_SCYLLA_URLS environment variable");
-    process.exit(1);
+  console.error("Please set the COMPOSE_SCYLLA_URLS environment variable");
+  process.exit(1);
 }
 
 // your environment variable for the maps should look like:
@@ -57,29 +57,32 @@ let authProvider = new cassandra.auth.PlainTextAuthProvider(username, password);
 let uuid = require("uuid");
 
 let client = new cassandra.Client({
-    contactPoints: translator.getContactPoints(),
-    policies: {
-        addressResolution: translator
-    },
-    authProvider: authProvider,
-    sslOptions: sslopts
+  contactPoints: translator.getContactPoints(),
+  policies: {
+    addressResolution: translator
+  },
+  authProvider: authProvider,
+  sslOptions: sslopts
 });
 
 // Add a word to the database
 function addWord(word, definition) {
-    return client.execute(
-        "INSERT INTO grand_tour.words(my_table_id, word, definition) VALUES(?,?,?)", [uuid.v4(), word, definition], {
-            prepare: true
-        });
+  return client.execute(
+    "INSERT INTO grand_tour.words(my_table_id, word, definition) VALUES(?,?,?)",
+    [uuid.v4(), word, definition],
+    {
+      prepare: true
+    }
+  );
 }
 
 // Get words from the database
 function getWords() {
-    return client.execute("SELECT * FROM grand_tour.words").then(result => {
-        return new Promise((resolve, reject) => {
-            resolve(result.rows);
-        });
+  return client.execute("SELECT * FROM grand_tour.words").then(result => {
+    return new Promise((resolve, reject) => {
+      resolve(result.rows);
     });
+  });
 }
 
 // We can now set up our web server. First up we set it to serve static pages
@@ -88,47 +91,47 @@ app.use(express.static(__dirname + "/public"));
 // The user has clicked submit to add a word and definition to the database
 // Send the data to the addWord function and send a response if successful
 app.put("/words", (request, response) => {
-    addWord(request.body.word, request.body.definition)
-        .then((resp) => {
-            response.status(200).send(resp);
-        })
-        .catch(function (err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
+  addWord(request.body.word, request.body.definition)
+    .then(resp => {
+      response.status(200).send(resp);
+    })
+    .catch(function(err) {
+      console.log(err);
+      response.status(500).send(err);
+    });
 });
 
 // Read from the database when the page is loaded or after a word is successfully added
 // Use the getWords function to get a list of words and definitions from the database
-app.get("/words",  (request, response) => {
-    getWords()
-        .then((words) => {
-            response.send(words);
-        })
-        .catch((err) => {
-            console.log(err);
-            response.status(500).send(err);
-        });
+app.get("/words", (request, response) => {
+  getWords()
+    .then(words => {
+      response.send(words);
+    })
+    .catch(err => {
+      console.log(err);
+      response.status(500).send(err);
+    });
 });
 
 console.log("Connecting");
 
 // create a keyspace and a table if they don't already exist
 client
-    .execute(
-        "CREATE KEYSPACE IF NOT EXISTS grand_tour WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' };"
-    )
-    .then(result => {
-        return client
-            .execute(
-                "CREATE TABLE IF NOT EXISTS grand_tour.words (my_table_id uuid, word text, definition text, PRIMARY KEY(my_table_id));"
-            );
-    }).then(result => {
-        app.listen(port, function () {
-            console.log("Server is listening on port " + port);
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        process.exit(1);
+  .execute(
+    "CREATE KEYSPACE IF NOT EXISTS grand_tour WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' };"
+  )
+  .then(result => {
+    return client.execute(
+      "CREATE TABLE IF NOT EXISTS grand_tour.words (my_table_id uuid, word text, definition text, PRIMARY KEY(my_table_id));"
+    );
+  })
+  .then(result => {
+    app.listen(port, function() {
+      console.log("Server is listening on port " + port);
     });
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
